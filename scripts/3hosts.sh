ac5a6a254a66a930236ea475020a16d7dc57c000
James Harris <james@rancher.com>
2015-02-09 12:19:11 -0700

    Modified 3hosts better container handling.

--- scripts/3hosts.sh
+++ scripts/3hosts.sh
@@ -2,13 +2,31 @@
 
 set -e
 
+CMDS="jq curl docker"
+
+for i in $CMDS
+do
+        # command -v will return >0 when the $i is not found
+	which $i >/dev/null && continue || { echo "$i command not found."; exit 1; }
+done
+
+function cattleServer(){
+#the magic to determine if cattle server needs to be run restarted or rebuilt.
+    if [[ $(docker inspect cattleserver | jq -r .[0].Id) == "null" ]]; then
+        docker run -d --privileged -p 8080:8080 --ip=172.17.0.20 --name=cattle cattleserver
+    else
+        docker restart cattleserver
+    fi
+    
+}
+
 cd $(dirname $0)/..
-docker build -t cattleserver .
+docker build --rm -t cattleserver .
 cd scripts/
 docker build -t hostcontainer .
 
-docker rm  -fv $(docker ps -qa) | echo > /dev/null
-docker run -d --privileged -p 8080:8080 --name=cattle cattleserver
-docker run -d --privileged --name=host1 hostcontainer
-docker run -d --privileged --name=host2 hostcontainer
-docker run -d --privileged --name=host3 hostcontainer
+docker rm  -fv $(docker ps -qa)
+cattleServer
+docker rm -vf host1  | echo > /dev/null;docker run -d --privileged -p 9345:9345 --name=host1 --ip=172.17.0.21 hostcontainer
+docker rm -vf host2  | echo > /dev/null;docker run -d --privileged -p 9345:9345 --name=host2 --ip=172.17.0.22 hostcontainer
+docker rm -vf host3  | echo > /dev/null;docker run -d --privileged -p 9345:9345 --name=host3 --ip=172.17.0.23 hostcontainer
