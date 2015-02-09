2ee44f51079464467291ee94e74dc6d8485ede89
James Harris <james@rancher.com>
2015-02-09 15:15:04 -0700

    Restart not start.

--- scripts/3hosts.sh
+++ scripts/3hosts.sh
@@ -27,8 +27,8 @@ function create_hosts(){
 }
 
 function start_stuff(){
-    docker start host1 host2 host3
-    docker start cattle
+    docker restart host1 host2 host3
+    docker restart cattle
     
 }
 
