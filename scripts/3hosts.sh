a12053c78d396eeb7ab4fe0d1d0aeec41d436973
James Harris <james@rancher.com>
2015-02-09 11:29:23 -0700

    Alter 3hosts

--- scripts/3hosts.sh
+++ scripts/3hosts.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-docker build -t test .
+cd $(dirname $0)/..
+docker build -t cattleserver .
+cd scripts/
+docker build -t hostcontainer .
+
 docker rm  -fv $(docker ps -qa) | echo > /dev/null
-docker run -d --privileged --name=cattle test
-docker run -d --privileged --name=host1 test
-docker run -d --privileged --name=host2 test
-docker run -d --privileged --name=host3 test
+docker run -d --privileged -p 8080:8080 --name=cattle cattleserver
+docker run -d --privileged --name=host1 hostcontainer
+docker run -d --privileged --name=host2 hostcontainer
+docker run -d --privileged --name=host3 hostcontainer
