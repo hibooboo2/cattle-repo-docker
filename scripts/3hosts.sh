d69c3ebf7416678e0d216d463313369db75b6dc7
James Harris <james@rancher.com>
2015-02-09 14:38:18 -0700

    Changing to use create and start.

--- scripts/3hosts.sh
+++ scripts/3hosts.sh
@@ -10,13 +10,21 @@ do
 	which $i >/dev/null && continue || { echo "$i command not found."; exit 1; }
 done
 
-function cattleServer(){
+function cattle_server(){
 #the magic to determine if cattle server needs to be run restarted or rebuilt.
-    if [[ $(docker inspect cattle | jq -r .[0].Name | echo) != "/cattle" ]]; then
-        docker run -d --privileged -p 8080:8080 --ip=172.17.0.20 --name=cattle cattleserver
-    else
-        docker restart cattle
-    fi
+    docker rm -vf cattle | echo > /dev/null;docker create --privileged -p 8080:8080 --name=cattle cattleserver
+        
+}
+
+function create_hosts(){
+    docker rm -vf host1  | echo > /dev/null;docker create --privileged -p 9345:9345 --name=host1 hostcontainer
+    docker rm -vf host2  | echo > /dev/null;docker create --privileged -p 9345:9345 --name=host2 hostcontainer
+    docker rm -vf host3  | echo > /dev/null;docker create --privileged -p 9345:9345 --name=host3 hostcontainer
+}
+
+function start_stuff(){
+    docker start host1 host2 host3
+    docker start cattle
     
 }
 
@@ -24,7 +32,6 @@ cd $(dirname $0)/..
 docker build --rm -t cattleserver .
 cd scripts/
 docker build -t hostcontainer .
-cattleServer
-docker rm -vf host1  | echo > /dev/null;docker run -d --privileged -p 9345:9345 --name=host1 --ip=172.17.0.21 hostcontainer
-docker rm -vf host2  | echo > /dev/null;docker run -d --privileged -p 9345:9345 --name=host2 --ip=172.17.0.22 hostcontainer
-docker rm -vf host3  | echo > /dev/null;docker run -d --privileged -p 9345:9345 --name=host3 --ip=172.17.0.23 hostcontainer
+create_hosts
+cattle_server
+start_stuff
