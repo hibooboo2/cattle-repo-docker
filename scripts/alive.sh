56bac6a576db734e08da13375aaa0dae40164008
James Harris <james@rancher.com>
2015-02-09 22:16:12 -0700

    almost run agent.

--- scripts/alive.sh
+++ scripts/alive.sh
@@ -5,10 +5,13 @@ set -e
 wrapdocker
 
 number=0
-while sleep 5
+while [ -z $(curl cattle:8080/v1/hosts) ]
 do
+    sleep 5
     echo $number
     number=$(($number + 1))
     echo $(curl cattle:8080/v1/hosts)
     docker ps -a
 done
+
+echo Now start the agent.
