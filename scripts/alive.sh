367c013a5853b3384df9b8b715b20721cef0d11e
James Harris <james@rancher.com>
2015-02-09 22:31:13 -0700

    garbage

--- scripts/alive.sh
+++ scripts/alive.sh
@@ -18,4 +18,5 @@ echo Now start the agent.
 docker run -d --privileged -v /var/lib/docker:/var/lib/docker -v /var/run/docker.sock:/var/run/docker.sock rancher/agent http://cattle:8080/v1/scripts/A5D504A17F9646C27F73:1423544400000:Vcq98xfUKoXvVTSWKg3BuWh4XFc
 while sleep 10
 do
+    echo skhfkjhf > /dev/null
 done
