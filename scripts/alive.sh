906b7ab3e584c2d2418171a2bcaccc2846034527
James Harris <james@rancher.com>
2015-02-10 23:48:15 -0700

    Better alive

--- scripts/alive.sh
+++ scripts/alive.sh
@@ -6,10 +6,8 @@ number=0
 while [ -z $(curl cattle:8080/v1/hosts) ]
 do
     sleep 5
-    echo $number
+    echo Connection attempts $number
     number=$(($number + 1))
-    echo $(curl cattle:8080/v1/hosts)
-    docker ps -a
 done
 
 echo Now start the agent.
