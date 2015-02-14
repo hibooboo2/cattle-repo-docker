1f0227b7323024ec5db51bedf928ea0b48818b05
James Harris <james@rancher.com>
2015-02-14 00:51:50 -0700

    Added validation magic.

---
+++ rancher_validation/build_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+function create_hosts(){
+    cattleip=$(docker inspect sleepy_carson | jq -r .[0].NetworkSettings.IPAddress)
+    for i in {1..3}
+    do 
+        docker rm -vf host$i  | echo > /dev/null;docker run -v /var/tmp/rancher/host$ienv/:/var/lib/docker --privileged --name=host$i rancher/node-simulator $cattleip:8080
+#        docker rm -vf host$i  | echo > /dev/null;docker create -v /var/tmp/rancher/host$ienv/:/var/lib/docker --link=cattle:cattle --privileged --name=host$i hostcontainer
+    done
+}
+
+cd $(dirname $0)/..
+docker build --rm -t cattleservercontainer .
+docker restart cattle || docker create --privileged -v /var/tmp/rancher/masterenv/:/var/lib/docker -p 8080:8080 --name=cattle cattleservercontainer
+create_hosts
+while sleep 10
+do
+    docker logs -f cattle
+done
